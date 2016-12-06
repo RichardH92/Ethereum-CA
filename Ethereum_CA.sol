94c3f4bd5faaec8a0aa61c1035028b9045883e54
Richard Hart <richardhart92@gmail.com>
2016-12-06 09:12:01 -0500

    Update Ethereum_CA.sol

--- Ethereum_CA.sol
+++ Ethereum_CA.sol
@@ -4,13 +4,15 @@ contract Ethereum_CA {
     struct Cert {
         uint public_key;
         string domain_name;
+        string message;
     }
 
     mapping(address => Cert) certs;
 
-    function register_cert(uint _pub_key, string _domain_name) {
+    function register_cert(uint _pub_key, string _domain_name, string _message) {
         certs[msg.sender].public_key = _pub_key;
         certs[msg.sender].domain_name = _domain_name;
+        certs[msg.sender].message = _message;
     }
 
     function get_public_key(address owner) constant returns (uint key) {
@@ -19,5 +21,9 @@ contract Ethereum_CA {
 
     function get_domain_name(address owner) constant returns (string name) {
         name = certs[owner].domain_name;
+    }
+    
+    function get_message(address owner) constant returns (string message) {
+        message = certs[owner].message;
     }
 }
